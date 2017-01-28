6560f11553d6038a2f1544eaf0f27dcd6c71bb98
Veronica Kang <vkang02@gmail.com>
2017-01-27 19:42:17 -0800

    automatically opens and shows results in a webpage called query.html

--- myJobSearch.py
+++ myJobSearch.py
@@ -8,6 +8,10 @@ import urllib2
 import json
 import csv
 import requests
+import sys
+import os
+import string
+import webbrowser
 
 # the function to search for a job (query). It will currently print out the job title, the company name, 
 # location, and url to the job description in a csv file called "query you input".csv
@@ -26,6 +30,7 @@ excludeWords = raw_input("List any words that should not be in the job title and
 #glassdoorUrl = 'http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=118132&t.k=f7epqLmorMI&action=jobs&q=web+developer&userip=10.0.0.7&useragent=Chrome/55.0.2883.95'
 #countries = set(['us','ar','au','at','bh','be','br','ca','cl','cn','co','cz','dk','fi','fr','de','gr','hk','hu','in','id','ie','il','it','jp','kr','kw','lu','my','mx','nl','nz','no','om','pk','pe','ph','pl','pt','qa','ro','ru','sa','sg','za','es','se','ch','tw','tr','ae','gb','ve'])
 num = 0
+new = 2
 myLocs = location.split(",")
 exclude = excludeWords.split(",")
 #exclude = ("senior", "Senior", "sr", "Sr", "Sr.", "sr.")
@@ -55,9 +60,27 @@ with open(query + ".csv", "a") as csvFile:
 					continue
 				else:
 					results.writerow([item['formattedRelativeTime'].encode('utf-8'), item['date'].encode('utf-8'), item['jobtitle'].encode('utf-8'), item['company'].encode('utf-8'), item['formattedLocation'].encode('utf-8'), item['url'].encode('utf-8'), item['jobkey'].encode('utf-8')])
+	csvFile.close();
 
+with open(query + ".csv", "rb") as csvFile:
+	
+	f = open(query + ".html", "w")
+	table_string = "<table>"
+	reader = csv.reader( csvFile )
+	for row in reader:
+		table_string += "<tr>" + \
+						"<td>" + \
+						string.join( row, "</td><td>" ) + \
+						"</td>" + \
+						"</tr>\n"
+	table_string += "</table>"
+	    
+	f.write(table_string)
+	#sys.stdout.write( table_string ) 
+	# open an HTML file on my own (Windows) computer
+	url = "file:///Users/vkang/Desktop/JobSearchScript/" + query + ".html"
+	webbrowser.open(url,new=new)
 
-
-
+	f.close()
 
 
