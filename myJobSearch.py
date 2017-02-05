55024ce0cc78fe2a1b00547ef7ad3afbbe7f33d2
Veronica Kang <vkang02@gmail.com>
2017-02-05 14:27:44 -0800

    some styling and column adjustment

--- myJobSearch.py
+++ myJobSearch.py
@@ -19,6 +19,8 @@ import webbrowser
 
 # the url for the Indeed Api with the following publisher id: 8145859988824678
 indeedUrl = 'http://api.indeed.com/ads/apisearch?publisher=8145859988824678&v=2&limit=100&format=json'
+glassdoorUrl = 'http://api.glassdoor.com/api/api.htm?t.p=118132&t.k=f7epqLmorMI&userip=0.0.0.0&useragent=&format=json&v=1&action=employers'
+#glassdoorUrl = 'http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=118132&t.k=f7epqLmorMI&action=jobs&q=web+developer&userip=10.0.0.7&useragent=Chrome/55.0.2883.95'
 
 query = raw_input("What kind of job are you looking for? i.e. 'Software Engineer' ")
 location = raw_input("Where do you want to work? i.e. 'San Francisco, Seattle, ...' ")
@@ -28,19 +30,19 @@ companyType = raw_input("Do you want to only show public companies? (True/False)
 #maxRes = raw_input("How many results would you like to see for each location? ")
 
 
-#glassdoorUrl = 'http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=118132&t.k=f7epqLmorMI&action=jobs&q=web+developer&userip=10.0.0.7&useragent=Chrome/55.0.2883.95'
 #countries = set(['us','ar','au','at','bh','be','br','ca','cl','cn','co','cz','dk','fi','fr','de','gr','hk','hu','in','id','ie','il','it','jp','kr','kw','lu','my','mx','nl','nz','no','om','pk','pe','ph','pl','pt','qa','ro','ru','sa','sg','za','es','se','ch','tw','tr','ae','gb','ve'])
 num = 0
 new = 2
 myLocs = location.split(",")
 exclude = excludeWords.split(",")
 #exclude = ("senior", "Senior", "sr", "Sr", "Sr.", "sr.")
-	
-grabforNum = requests.get(indeedUrl)
-json_content = json.loads(grabforNum.content)
-print(json_content["totalResults"])
 
-totalres = (json_content["totalResults"])
+# #Creates 
+# grabforNum = requests.get(indeedUrl)
+# json_content = json.loads(grabforNum.content)
+# print(json_content["totalResults"])
+# totalres = (json_content["totalResults"])
+
 
 def column(matrix, i):
     return [row[i] for row in matrix]
@@ -51,7 +53,6 @@ with open(query + ".csv", "a") as csvFile:
 			theList = list(reader)
 
 			results = csv.writer(csvFile)
-			#results.writerow(['Post Age', 'Date Created', 'Job Title', 'Company', 'Location', 'URL', 'Job Key'])
 			
 			for l in myLocs:
 				for num in range(0, 100, 10):
@@ -64,43 +65,45 @@ with open(query + ".csv", "a") as csvFile:
 					print 'Complete '+ final_url
 
 					for item in data['results']:
+						#If any of the excluded words are in the job title, continue and don't add the job title to the results
 						if any(lvl in item['jobtitle'].encode('utf-8') for lvl in exclude):	
 							continue
+						#If the user wants to see only public companies from the S&P 500 List, display only those results
 						elif(companyType == "True" or companyType == "true" or companyType =="t" or companyType =="T"):
 							if(item['company'].encode('utf-8') in column(theList, 1)):
-								results.writerow([item['formattedRelativeTime'].encode('utf-8'), item['date'].encode('utf-8'), item['jobtitle'].encode('utf-8'), item['company'].encode('utf-8'), item['formattedLocation'].encode('utf-8'), item['url'].encode('utf-8'), item['jobkey'].encode('utf-8')])
+								results.writerow([item['formattedRelativeTime'].encode('utf-8'), item['jobtitle'].encode('utf-8'), item['company'].encode('utf-8'), item['formattedLocation'].encode('utf-8'), item['url'].encode('utf-8')])
+						#Display everything 
 						else:
-							results.writerow([item['formattedRelativeTime'].encode('utf-8'), item['date'].encode('utf-8'), item['jobtitle'].encode('utf-8'), item['company'].encode('utf-8'), item['formattedLocation'].encode('utf-8'), item['url'].encode('utf-8'), item['jobkey'].encode('utf-8')])
+							results.writerow([item['formattedRelativeTime'].encode('utf-8'), item['jobtitle'].encode('utf-8'), item['company'].encode('utf-8'), item['formattedLocation'].encode('utf-8'), item['url'].encode('utf-8')])
 		csvFile.close();
 
 with open(query + ".csv", "rb") as csvFile:
 	
 	f = open(query + ".html", "w")
-	table_string = "<table><tr><th>Post Age</th><th>Date Created</th><th>Job Title</th><th>Company</th><th>Location</th></tr>"
+	table_string = "<html><head><title>WhatNow</title><link rel='stylesheet' type='text/css' href='myJobSearch.css'><link href='https://fonts.googleapis.com/css?family=Arvo' rel='stylesheet'><link rel='stylesheet' type='text/css' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css'></head><body>"
+	table_string += "<table class='table table-bordered'><tr><th>Post Age</th><th>Job Title</th><th>Company</th><th>Location</th></tr>"
 	reader = csv.reader( csvFile )
+	#Create a table in the html file. Make the Job title a link to the actual job post
 	for row in reader:
 		table_string += "<tr>" + \
 						"<td>" +\
 						row[0] + \
 						"</td><td>" + \
-						row[1] + \
-						"</td><td>" + \
 						"<a href='" + \
-						row[5]+ \
+						row[4]+ \
 						"'>" + \
-						row[2] + \
+						row[1] + \
 						"</a>" + \
 						"</td><td>" + \
-						row[3] +\
+						row[2] +\
 						"</td><td>" + \
-						row[4] + \
+						row[3] + \
 						"</td><td></td>" + \
 						"</tr>\n"
-	table_string += "</table>"
+	table_string += "</table></body>"
 	    
 	f.write(table_string)
-	#sys.stdout.write( table_string ) 
-	# open an HTML file on my own (Windows) computer
+	# open an HTML file on my computer
 	url = "file:///Users/vkang/Desktop/JobSearchScript/" + query + ".html"
 	webbrowser.open(url,new=new)
 
